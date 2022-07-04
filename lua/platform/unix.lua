ac21b41f820ff3397206b3cbf5bac825730b8136
LevyCory <corylevy@icloud.com>
2022-07-04 14:11:45 -0400

    Add unix platform settings

---
+++ lua/platform/unix.lua
@@ -0,0 +1,21 @@
+return {
+    language_servers = {
+        sumneko_lua = {
+            enabled = true,
+            cmd = {
+                '/usr/local/bin/lua-language-server/bin/lua-language-server',
+                '-E',
+                '/usr/local/bin/lua-language-server/bin/main.lua'
+            }
+        },
+        clangd = {
+            enabled = true
+        },
+        jedi_language_server = {
+            enabled = true,
+            cmd = {
+                '~/.local/bin/jedi-language-server'
+            },
+        }
+    }
+}
