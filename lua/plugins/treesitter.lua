d8d36444509adadb7142345348f98138349cc340
Cory Levy <corylevy@gmail.com>
2023-05-28 11:36:45 -0400

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,37 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    event = { "BufReadPost", "BufNewFile" },
-    build = ":TSUpdate",
-    opts = {
-        ensure_installed = {
-            'bash',
-            'c',
-            'cpp',
-            'json',
-            'lua',
-            'luadoc',
-            'luap',
-            'markdown',
-            'markdown_inline',
-            'norg',
-            'python',
-            'regex',
-            'rust',
-            'vim',
-            'vimdoc',
-            'yaml',
-        },
-        highlight = {
-            enable = true, -- false will disable the whole extension
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true,
-        },
+    event = {
+        "BufReadPost",
+        "BufNewFile"
     },
+    build = ":TSUpdate",
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = {
+                'bash',
+                'c',
+                'cpp',
+                'json',
+                'lua',
+                'luadoc',
+                'luap',
+                'markdown',
+                'markdown_inline',
+                'norg',
+                'python',
+                'regex',
+                'rust',
+                'vim',
+                'vimdoc',
+                'yaml',
+            },
+            highlight = {
+                enable = true, -- false will disable the whole extension
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true,
+            },
+        }
+    end,
 }
