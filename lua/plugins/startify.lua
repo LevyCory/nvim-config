9a6b629140efacde5c9d2c2fc4246c1bd95f8ec6
Cory Levy <corylevy@icloud.com>
2024-12-29 12:15:33 -0500

    Add startify session persistency

--- lua/plugins/startify.lua
+++ lua/plugins/startify.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   config = function()
     vim.cmd [[
+            let g:startify_session_persistence = 1
             let g:startify_padding_left = 4
             let g:startify_custom_header = startify#pad([
                 \ '',
