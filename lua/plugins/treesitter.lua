5bcc3dcbe15ef4e237c8c7557b98a2f3ca2a4352
Cory Levy <corylevy@icloud.com>
2026-04-06 16:07:17 -0400

    fix-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,40 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  event = {
-    "BufReadPost",
-    "BufNewFile"
-  },
-  build = ":TSUpdate",
-  config = function()
-    require('nvim-treesitter').setup {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cmake',
-        'cpp',
-        'javascript',
-        'json',
-        'lua',
-        'luadoc',
-        'luap',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'regex',
-        'rust',
-        'vim',
-        'vimdoc',
-        'xml',
-        'yaml',
-      },
+  branch = 'main',
+  init = function()
+    local ensure_installed = {
+      'bash',
+      'c',
+      'cmake',
+      'cpp',
+      'javascript',
+      'json',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'regex',
+      'rust',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
     }
+
+    local installed_parsers = require('nvim-treesitter.config').get_installed()
+    local parsers_to_install = vim.iter(ensure_installed):filter(function(parser)
+      return not vim.tbl_contains(installed_parsers, parser)
+    end):totable()
+
+    require('nvim-treesitter').install(parsers_to_install)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
   end,
 }
